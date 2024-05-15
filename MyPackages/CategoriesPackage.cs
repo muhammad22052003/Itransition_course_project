e9c051ca19369f3edb91a87f5ea8dfc313ded80a
Muhammad <muhammadarch22@gmail.com>
2024-05-16 03:07:34 +0500

    Fixed folder name Packages to MyPackages

---
+++ MyPackages/CategoriesPackage.cs
@@ -0,0 +1,25 @@
+﻿using CourseProject_backend.CustomDbContext;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+
+namespace CourseProject_backend.Packages
+{
+    public static class CategoriesPackage
+    {
+        private static IEnumerable<string> CategoriesNames { get; set; } = new List<string>();
+
+        public async static Task Initialize(CollectionDBContext dBContext)
+        {
+            CategoriesNames = (await dBContext.Categories
+                .Where((c)=>true)
+                .ToListAsync())
+                .Select((c) => c.Name)
+                .ToArray();
+        }
+
+        public static IEnumerable<string> GetCategoriesNames()
+        {
+            return CategoriesNames;
+        }
+    }
+}
---
+++ MyPackages/LanguagePackSingleton.cs
@@ -0,0 +1,47 @@
+﻿using CourseProject_backend.Enums.Packages;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CourseProject_backend.Packages
+{
+    public class LanguagePackSingleton
+    {
+        private static LanguagePackSingleton _instance = null;
+
+        private LanguagePackSingleton()
+        {
+            Packages = new Dictionary<string, IDictionary<string, string>>();
+        }
+
+        public static LanguagePackSingleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new LanguagePackSingleton();
+            }
+
+            return _instance;
+        }
+
+        public IDictionary<string, string> GetLanguagePack(AppLanguage language)
+        {
+            if (!Packages.ContainsKey(language.ToString()))
+            {
+                StringBuilder json = new StringBuilder();
+
+                json = new StringBuilder(File.ReadAllText($"{language.ToString()}Pack.json"));
+
+                var jsonDocument = JsonDocument.Parse(json.ToString());
+
+                Packages.Add(key: language.ToString(),
+                             value: JsonSerializer
+                             .Deserialize<IDictionary<string, string>>(jsonDocument));
+            }
+
+            return Packages[language.ToString()];
+        }
+
+        private IDictionary<string, IDictionary<string, string>> Packages { get; set; }
+    }
+}
